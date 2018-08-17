3a091c738383e88b46fd76027b0a34c22350ab38
Danilo de Lima <danilolicd@gmail.com>
2018-08-17 16:17:25 -0300

    Adiciona crawler de departamentos

--- crawler.rb
+++ crawler.rb
@@ -6,6 +6,7 @@ require 'json'
 
 require './crawler_classes'
 require './crawler_courses'
+require './crawler_departments'
 
 # DOMÍNIO
 SITE = 'https://matriculaweb.unb.br/'
@@ -14,13 +15,18 @@ def menu
   puts 'Escolha uma opção:'
   puts '1 - Pegar as turmas do Darcy Ribeiro'
   puts '2 - Pegar as turmas de Planaltina'
-  puts '3 - Pegar as turmas de Ceilândia'
+  puts '3 - Pegar as turmas da Ceilândia'
   puts '4 - Pegar as turmas do Gama'
   puts '-------------------------------------------'
   puts '5 - Pegar os cursos do Darcy Ribeiro'
-  puts '6 - Pegar os cursos do Planaltina'
-  puts '7 - Pegar os cursos do Ceilândia'
+  puts '6 - Pegar os cursos de Planaltina'
+  puts '7 - Pegar os cursos da Ceilândia'
   puts '8 - Pegar os cursos do Gama'
+  puts '-------------------------------------------'
+  puts '9 - Pegar os departamentos do Darcy Ribeiro'
+  puts '10 - Pegar os departamentos de Planaltina'
+  puts '11 - Pegar os departamentos da Ceilândia'
+  puts '12 - Pegar os departamentos do Gama'
 
   a = gets.to_i
 
@@ -41,6 +47,14 @@ def menu
     crawler_courses(3, 'ceilandia_courses.json')
   when 8
     crawler_courses(4, 'gama_courses.json')
+  when 9
+    crawler_departments(1, 'darcy_departments.json')
+  when 10
+    crawler_departments(2, 'planaltina_departments.json')
+  when 11
+    crawler_departments(3, 'ceilandia_departments.json')
+  when 12
+    crawler_departments(4, 'gama_departments.json')
   else
     system 'clear' or system 'cls'
     menu
