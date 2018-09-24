27692830664ef8f0348f257cf84826f916087205
Danilo de Lima <danilolicd@gmail.com>
2018-09-24 17:30:45 -0300

    add level field to subjects scrap

--- lib/mwcrawler/subjects.rb
+++ lib/mwcrawler/subjects.rb
@@ -3,10 +3,8 @@ module Mwcrawler
   module Subjects
     def self.scrap(campus)
       page = Helpers.set_crawler(campus, 'graduacao/oferta_dep.aspx?cod=')
-      dep_links = page.css('#datatable tr td:nth-child(3) a')
-                      .map { |link| link['href'] }
-
-      dep_codes = page.css('#datatable tr td:nth-child(1)').map(&:text)
+      dep_links = department_links(page)
+      dep_codes = department_codes(page)
 
       rows = []
 
@@ -14,7 +12,7 @@ module Mwcrawler
         page = Nokogiri::HTML(URI.open(SITE + 'graduacao/' + link))
         rows << scrap_row(dep_codes[index], page)
       end
-      rows
+      rows.flatten!
     end
 
     private_class_method def self.scrap_row(dep_code, page)
@@ -23,13 +21,22 @@ module Mwcrawler
       length.times do |i|
         subjects << subject_row_init(page, dep_code, i)
       end
-      subjects unless subjects.empty?
+      subjects
     end
 
     private_class_method def self.subject_row_init(page, department, index)
       { code: page.css('#datatable tr td:nth-child(1)').map(&:text)[index],
         name: page.css('#datatable tr td:nth-child(2)').map(&:text)[index],
-        department: department }
+        department: department,
+        level: 'graduação' }
+    end
+
+    private_class_method def self.department_links(page)
+      page.css('#datatable tr td:nth-child(3) a').map { |link| link['href'] }
+    end
+
+    private_class_method def self.department_codes(page)
+      page.css('#datatable tr td:nth-child(1)').map(&:text)
     end
   end
 end
