b263b5fe70634069883e547ea1d44a1b1e1dc9cf
vitor pontes <vitormax2005@hotmail.com>
2018-10-01 23:27:30 -0300

    Change classes scraping param from campus to department

--- spec/mwcrawler_spec.rb
+++ spec/mwcrawler_spec.rb
@@ -4,16 +4,32 @@ RSpec.describe Mwcrawler do
   end
 
   describe 'Classes scrap' do
-    Mwcrawler::Campuses.all.each do |campus, _campus_id|
-      context "Classes campus: #{campus}" do
+    [116].each do |department_code|
+      context "Classes department: #{department_code}" do
+        let(:first_class) { @classes.first }
+
         before :all do
-          VCR.use_cassette("classes_#{campus}") do
-            @classes = Mwcrawler::Crawler.new.classes(campus)
+          VCR.use_cassette("classes_#{department_code}") do
+            @classes = Mwcrawler::Crawler.new.classes(department_code)
           end
         end
 
-        it 'crawls classes' do
-          expect(@classes).to be_a_kind_of Array
+        it { expect(@classes).to be_a_kind_of Array }
+        it { expect(@classes).to_not be_empty }
+
+        it 'has the correct keys' do
+          expect(first_class.keys).to include(:department, :code, :course_code, :name, :schedules, :teachers, :credits)
+        end
+
+        it { expect(first_class[:code]).to be_a_kind_of Integer }
+        it { expect(first_class[:course_code]).to be_a_kind_of Integer }
+        it { expect(first_class[:schedules]).to be_a_kind_of Array }
+        it { expect(first_class[:teachers]).to be_a_kind_of Array }
+        it { expect(first_class[:credits]).to be_a_kind_of Hash }
+        it { expect(first_class[:credits].keys).to include(:theory, :practical, :extension, :study) }
+
+        %i[theory practical extension study].each do |credit_type|
+          it { expect(first_class[:credits][credit_type]).to be_a_kind_of Integer }
         end
       end
     end
