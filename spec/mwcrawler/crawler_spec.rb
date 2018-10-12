bf118bc3e6a2f56cdb542a68ce395f3a0e60a350
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2018-10-12 15:28:54 -0300

    better use of rspec tools

--- spec/mwcrawler/crawler_spec.rb
+++ spec/mwcrawler/crawler_spec.rb
@@ -1,8 +1,7 @@
 RSpec.describe Mwcrawler::Crawler do
   it '#semester returns the current semester' do
     VCR.use_cassette("graduacao_default") do
-      crawler = Mwcrawler::Crawler.new
-      expect(crawler.semester).to eq('2018/2')
+      expect(subject.semester).to eq('2018/2')
     end
   end
 end
