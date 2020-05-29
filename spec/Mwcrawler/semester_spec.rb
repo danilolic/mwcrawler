2bf3efb39eba6db7ebb27538a22e437ba877b32d
Danilo de Lima <danilolicd@gmail.com>
2020-05-29 17:24:09 -0300

    organize folder structure to specs

--- spec/Mwcrawler/semester_spec.rb
+++ spec/Mwcrawler/semester_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe Mwcrawler::Crawler do
   it '#semester returns the current semester' do
-    VCR.use_cassette('graduacao_default') do
+    VCR.use_cassette('semester/graduacao_default') do
       expect(subject.semester).to eq('2018/2')
     end
   end
